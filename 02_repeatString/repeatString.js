8a1b8ed9bbd19e3947dcc859a449974f6771c4e8
MKdivine <jensboerger23@gmail.com>
2024-09-15 18:34:23 +0200

    hello

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (str, num1) {
+  let result = "";
+  for (let i = 0; i < num1; i++) {
+    result += str;
+  }
+  return result;
 };
-
 // Do not edit below this line
 module.exports = repeatString;

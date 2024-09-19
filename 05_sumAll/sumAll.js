30b7557eb21004bf6ace1f721a56473b4550a20d
MKdivine <jensboerger23@gmail.com>
2024-09-19 21:07:41 +0200

    solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,14 +1,19 @@
 const sumAll = function (num1, num2) {
     let sum = 0;
-
-    for (let i = num1; i <= num2; i++) {
+  
+    if (num1 > num2) {
+      for (let i = num1; i >= num2; i--) {
         sum += i;
-
-
+      }
+    } else {
+      for (let i = num1; i <= num2; i++) {
+        sum += i;
+      }
     }
-
+  
     return sum;
-}
+  }
+  
 
 
 // Do not edit below this line

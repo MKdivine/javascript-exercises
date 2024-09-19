7639cb2cce7bf473e7188fe4d34dd48fd90c6109
MKdivine <jensboerger23@gmail.com>
2024-09-19 21:28:54 +0200

    j00

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,9 @@
 const sumAll = function (num1, num2) {
     let sum = 0;
   
-    if (num1 > num2) {
+    if (num1 < 0 || num2 < 0 || typeof num1 === 'string' || typeof num2 === 'string' ||  (num1 % 1  === 0) === false || (num2 % 1  === 0) === false) {
+      return "ERROR";
+    } else if (num1 > num2) {
       for (let i = num1; i >= num2; i--) {
         sum += i;
       }
@@ -10,7 +12,7 @@ const sumAll = function (num1, num2) {
         sum += i;
       }
     }
-  
+  // scho
     return sum;
   }
   

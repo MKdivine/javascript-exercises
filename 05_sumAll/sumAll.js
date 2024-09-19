acdfdb775882f4a6aef52e6702cc0cae502fa93a
MKdivine <jensboerger23@gmail.com>
2024-09-19 04:27:24 +0200

    its getting there

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,15 @@
-const sumAll = function (...nums) {
+const sumAll = function (num1, num2) {
     let sum = 0;
 
-    nums.forEach(element => {
-        sum += element;
-    });
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+
+
+    }
+
     return sum;
-};
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;

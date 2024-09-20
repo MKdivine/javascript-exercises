a036085f97c648a964298f2ce8266d7aeada052c
MKdivine <jensboerger23@gmail.com>
2024-09-20 02:33:26 +0200

    lol

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
-
+const leapYears = function (num) {
+  if (num % 400 === 0) {
+    return true;
+  } else if (num % 100 === 0) {
+    return false;
+  }
+  if (num % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
-
 // Do not edit below this line
 module.exports = leapYears;
+
+/* 
+const leapYears = num => {
+  if (num % 400 === 0) return true;
+  if (num % 100 === 0) return false;
+  return num % 4 === 0;
+};
+ */

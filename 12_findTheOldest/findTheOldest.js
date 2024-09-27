8999e991a3dd99f0bddde6126877a22d24f5243e
MKdivine <jensboerger23@gmail.com>
2024-09-27 04:08:34 +0200

    lol

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+    let oldest = [];
 
+
+  array.forEach((element) => {
+    let yearOfBirth = element.yearOfBirth;
+    let yearOfDeath = element.yearOfDeath;
+    let age = yearOfDeath - yearOfBirth;
+    oldest.push(age, element.name);
+    console.log(Math.max.apply(null, oldest)); // logs 5
+});
 };
 
 // Do not edit below this line

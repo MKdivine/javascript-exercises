9a265eecb1ec30890474de8b4619baf25b455cb3
MKdivine <jensboerger23@gmail.com>
2024-09-27 02:34:15 +0200

    scho

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
 
+let newBooks = []
+const getTheTitles = function(arr) {
+    arr.forEach(element => {
+        newBooks.push(element.title)
+    });
+    return newBooks;
 };
 
 // Do not edit below this line

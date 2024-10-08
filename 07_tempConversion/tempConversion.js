8887f710c7612274d76192f5c2c14a71b68401c1
MKdivine <jensboerger23@gmail.com>
2024-10-08 04:01:45 +0200

    works

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let celsis = (num - 32) * 5 / 9;
+  let rounded = Math.round(celsis * 10) / 10
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let fahr = (num * 9 / 5) + 32;
+  let rounded = Math.round(fahr * 10) / 10
+  return rounded
 };
 
 // Do not edit below this line
